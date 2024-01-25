57a58a229cdcd0178c089dbbae3914614b044d47
arsOzaki <ozaki@ars-cube.com>
2024-01-25 17:56:11 +0900

    Firebase Authenticationを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:berth_app/ui/login_page.dart';
 import 'package:berth_app/ui/search_arrival_page.dart';
 import 'package:berth_app/util/size_config.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
