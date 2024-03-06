12134fa9ddaa06709b6c6cf64c3b5d33b5ddebd7
arsOzaki <ozaki@ars-cube.com>
2024-03-06 10:26:10 +0900

    同日でも過去日と判定されてしまう不具合を修正

--- lib/util/csv_reader.dart
+++ lib/util/csv_reader.dart
@@ -227,8 +227,10 @@ class CsvReader {
       return false;
     }
 
-    //日付が今日より過去の日付であることを確認
-    if (DateTime(year, month, day).isBefore(DateTime.now())) {
+    //日付が今日より過去の日付であることを確認(時間部分は省略）
+    final now = DateTime.now();
+    final date = DateTime(year, month, day);
+    if (date.isBefore(DateTime(now.year, now.month, now.day))) {
       return false;
     }
 
