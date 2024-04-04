56a0e1efa381c63fda22e0bfd96e43642bbb2569
arsOzaki <ozaki@ars-cube.com>
2024-04-04 11:10:14 +0900

    isNewの初期値をtrueに変更

--- lib/controller/confirm_data_controller.dart
+++ lib/controller/confirm_data_controller.dart
@@ -72,7 +72,7 @@ class ConfirmDataController extends StateNotifier<Future<CsvDataResult>> {
             'userCode': data.csvData[i].userCode,
             'deliveryPort': data.csvData[i].deliveryPort,
             'userName': data.csvData[i].userName,
-            'isNew': false,
+            'isNew': true,
           }).onError(
               (error, stackTrace) => throw Exception([error, stackTrace]));
         } catch (e, s) {
